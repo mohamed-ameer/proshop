a213ff93b5050911c49a31ae6f7051f6e38fe98b
mohamed ameer <mohamedameer577@gmail.com>
2023-07-15 19:50:16 +0300

    Register screen and functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../assets/logo.png';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { useLogoutMutation } from '../slices/userApiSlice';
+import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
 
 const Header = () => {
