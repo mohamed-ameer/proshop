e0375d515a01cf3b0cab1ecf4ec74c2d7c2acf34
mohamed ameer <mohamedameer577@gmail.com>
2023-08-11 16:28:22 +0300

    deploy preparing

--- backend/server.js
+++ backend/server.js
@@ -48,8 +48,9 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
+  //set the static folder (react build folder with all our static assets) 
   app.use(express.static(path.join(__dirname, '/frontend/build')));
-
+  //any route that is not api will be redirected to index.html
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
